64e985776d06d393176f6c0fbbc9a8cebe0ca17f
Rustam Sadykov <sadikov91@gmail.com>
2023-07-02 16:44:20 +0500

    Initial Commit

---
+++ Simple Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Counter
+//
+//  Created by Rustam Sadykov on 02.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
